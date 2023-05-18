552f88ec7069ca773fc3b5cffd5560834d68adf1
Fedosz <fedya.zhukov.2014@mail.ru>
2023-05-18 20:08:11 +0300

    Qualifier test

--- spring-1/src/main/java/ru/zhukov/springcourse/MusicPlayer.java
+++ spring-1/src/main/java/ru/zhukov/springcourse/MusicPlayer.java
@@ -1,18 +1,20 @@
 package ru.zhukov.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic music;
+    private Music music1;
+    private Music music2;
 
-    @Autowired
-    public MusicPlayer(ClassicalMusic music) {
-        this.music = music;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,@Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
     public String playMusic() {
-        return "Playing " + music.getSong();
+        return "Playing " + music1.getSong() + ", " + music2.getSong();
     }
 
 }
