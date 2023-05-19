a04ad66f430810596d656046d4337c9790cd9a21
Fedosz <fedya.zhukov.2014@mail.ru>
2023-05-19 19:00:44 +0300

    Added Post, Patch, Delete functions

---
+++ SpringAppREST/src/main/java/ru/zhukov/springcourse/models/Person.java
@@ -0,0 +1,29 @@
+package ru.zhukov.springcourse.models;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Person() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
